2541e4d9ecfca5734b0612b0fc510ea2211617d1
Brett Evrist <brettevrist10@gmail.com>
2023-05-23 14:23:45 -0700

    update keepass location

--- pc/Macos/dev/backup/keepass-update.sh
+++ pc/Macos/dev/backup/keepass-update.sh
@@ -6,7 +6,7 @@ NOW=$(date +"%Y_%m_%d")
 # Copy locally
 GOOGLE_DRIVE_ROOT_PATH="/Users/bevrist/Library/CloudStorage/GoogleDrive-brettevrist10@gmail.com/My Drive/"
 mkdir -p ~/.backup/
-cp "$GOOGLE_DRIVE_ROOT_PATH/_KeePass/BrettsPassDatabase.kdbx" ~/.backup/backup/BrettsPassDatabase.kdbx
+cp "$GOOGLE_DRIVE_ROOT_PATH/Personal Files/BrettsPassDatabase.kdbx" ~/.backup/backup/BrettsPassDatabase.kdbx
 
 ssh -p 2222 bevrist@play.brettevrist.net "mv /www/brettevrist.net/share/keePass/*.kdbx /www/brettevrist.net/share/keePass/_OLD/ || true"
 scp -P 2222 ~/Google\ Drive/My\ Drive/_KeePass/BrettsPassDatabase.kdbx bevrist@play.brettevrist.net:/www/brettevrist.net/share/keePass/BrettsPassDatabase_"$NOW".kdbx
