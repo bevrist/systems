894cc17c7cae30e88edb6f0df2f2df18cd38c1fa
Brett Evrist <brettevrist10@gmail.com>
2023-08-03 23:33:56 -0700

    update podman init script

--- pc/Macos/dev/podman-start.sh
+++ pc/Macos/dev/podman-start.sh
@@ -5,7 +5,10 @@ echo $VM_NAME
 
 # create machine if it doesnt exist
 if [ $(podman machine list | grep -P "^${VM_NAME}\*?\s" | wc -l) -ne 1 ]; then
-  podman machine init ${VM_NAME} --volume ${HOME}:${HOME}
+  podman machine init ${VM_NAME} \
+    --volume ${HOME}:${HOME} \
+    --cpus 4 \
+    --memory 4096
 fi
 
 # Start podman vm
