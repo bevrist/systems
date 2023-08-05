3f77574561d2cb0082ce53ba03774b94d7b1bfed
Brett Evrist <brettevrist10@gmail.com>
2023-08-05 00:05:11 -0700

    bump minecraft spigon-geyser versions

--- pc/Macos/dev/podman-start.sh
+++ pc/Macos/dev/podman-start.sh
@@ -7,8 +7,8 @@ echo $VM_NAME
 if [ $(podman machine list | grep -P "^${VM_NAME}\*?\s" | wc -l) -ne 1 ]; then
   podman machine init ${VM_NAME} \
     --volume ${HOME}:${HOME} \
-    --cpus 4 \
-    --memory 4096
+    --cpus 2 \
+    --memory 2048
 fi
 
 # Start podman vm
