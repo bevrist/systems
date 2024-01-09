df377432ea6b25cb4e45bf4d5cf5f69c65b412be
Brett Evrist <brettevrist10@gmail.com>
2024-01-08 22:29:00 -0800

    old file cleanup

--- pc/rsync-sync.sh
+++ pc/rsync-sync.sh
@@ -2,7 +2,7 @@
 COUNT=1
 while true
 do
-    rsync -rvzh --dry-run --delete --info=progress2 --append-verify --timeout=60 -e 'ssh -p 2215' bevrist@b.evri.st:/mnt/6TB-5400RPM/media /mnt/f/Plex-Backup_DATE
+    rsync -rvzh --dry-run --delete --info=progress2 --append-verify --timeout=60 -e 'ssh -p 2215' bevrist@b.evri.st:/mnt/6TB-5400RPM/media /mnt/f/Backup_DATE
     if [ "$?" = "0" ] ; then
         echo "rsync completed normally"
         exit
