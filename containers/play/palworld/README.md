1fab7f126955b9823651fa55b700c62081977d4a
Brett Evrist <brettevrist10@gmail.com>
2024-01-22 13:17:33 -0800

    create palworld server

---
+++ containers/play/palworld/README.md
@@ -0,0 +1,5 @@
+# Palworld Dedicated Server
+
+Update Palworld Assets: `UPDATE=1 docker-compose up`
+
+Run Palworld: `docker-compose up -d`
---
+++ containers/play/palworld/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+cp -f /config/PalWorldSettings.ini /app/palworld/Pal/Saved/Config/LinuxServer/PalWorldSettings.ini
+
+if [ ! -z $UPDATE ] || [ ! -d "/app/palworld" ]; then
+  steamcmd +force_install_dir /app/palworld +login anonymous +app_update 2394010 validate +quit
+  # fix `steamclient.so: cannot open shared object file: No such file or directory` error
+  mkdir -p ~/.steam/sdk64/
+  steamcmd +force_install_dir /tmp/steamworks +login anonymous +app_update 1007 +quit
+  cp /tmp/steamworks/linux64/steamclient.so ~/.steam/sdk64/
+  exit
+fi
+
+echo "starting: $(date)" | tee /app/palworld/startLog.txt
+/app/palworld/PalServer.sh -useperfthreads -NoAsyncLoadingThread -UseMultithreadForDS
