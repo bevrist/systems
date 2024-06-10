46b6e941d6b85ae5cc160fb3cd626f4fe2358e6f
Brett Evrist <brettevrist10@gmail.com>
2024-06-09 19:58:42 -0700

    update github backup list

--- pc/Macos/dev/backup/github-backup.sh
+++ pc/Macos/dev/backup/github-backup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# REPOS LIST Updated `Dec 11 2023` # NOTE: UPDATE THIS LIST!
+# REPOS LIST Updated `Jun 9 2024` # NOTE: UPDATE THIS LIST!
 REPOS="git@github.com:bevrist/advent-of-code.git
 git@github.com:bevrist/cloudflare-workers-go.git
 git@github.com:bevrist/golang-library-bin-sizes.git
@@ -7,7 +7,6 @@ git@github.com:bevrist/playground.git
 git@github.com:bevrist/resume.git
 git@github.com:bevrist/scouting-app.git
 git@github.com:bevrist/shell-compose.git
-git@github.com:bevrist/simple-notify.git
 git@github.com:bevrist/systems.git
 git@github.com:bevrist/workout-site.git"
 
